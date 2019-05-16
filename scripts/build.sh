d65e6e85481753b2ddd87b24a7a3debe4572ca17
Chris McCluskey <cmccluskey@digitaltrends.com>
2019-05-16 15:35:10 -0700

    Break out header and footer partials

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p docs/about docs/projects docs/resume docs/contact
-mustache ./pages/home.json ./views/default.mustache > docs/index.html
-mustache ./pages/about.json ./views/default.mustache > docs/about/index.html
-mustache ./pages/projects.json ./views/default.mustache > docs/projects/index.html
-mustache ./pages/resume.json ./views/resume.mustache > docs/resume/index.html
-mustache ./pages/contact.json ./views/default.mustache > docs/contact/index.html
+mustache ./pages/home.json -p ./views/header.mustache -p ./views/footer.mustache ./views/default.mustache > docs/index.html
+mustache ./pages/about.json -p ./views/header.mustache -p ./views/footer.mustache ./views/default.mustache > docs/about/index.html
+mustache ./pages/projects.json -p ./views/header.mustache -p ./views/footer.mustache ./views/default.mustache > docs/projects/index.html
+mustache ./pages/resume.json -p ./views/header.mustache -p ./views/footer.mustache ./views/resume.mustache > docs/resume/index.html
+mustache ./pages/contact.json -p ./views/header.mustache -p ./views/footer.mustache ./views/default.mustache > docs/contact/index.html
